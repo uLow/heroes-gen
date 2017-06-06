b036c4b0db8f23218db68f1d7b5151ec5177c9ad
Vitalijs Litvinovs <dev@codin.pro>
2017-06-06 12:34:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
+$source = preg_replace('/[^-a-z]/i', '', $_GET['src'] ?? 'marvel');
 
-$heroesFile = file_get_contents('heroes.json');
+$heroesFile = file_get_contents($source . '.json');
 $heroes = json_decode($heroesFile);
 
 $filterList = [
-    'lowercase' => function($name){ return strtolower($name); }, 
-    'uppercase' => function($name){ return strtoupper($name); }, 
-    'alpha' => function($name){ return preg_replace('/[^a-z]/gi', '', $name); },
+    'lowercase' => function($name){ return strtolower($name); },
+    'uppercase' => function($name){ return strtoupper($name); },
+    'join' => function($name){ return str_replace(' ', '-', $name); },
+    'alpha' => function($name){ return preg_replace('/[^-a-z]|(?:[^a-z]$)/i', '', $name); },
 ];
 
 $hero = $heroes[mt_rand(0, count($heroes) - 1)];
 
 if(isset($_GET['filters'])){
     $filters = explode(',', $_GET['filters']);
-    
     foreach($filters as $filter){
         if(isset($filterList[$filter])){
             // We can't do `$hero = $filterList[$filter]($hero);`
