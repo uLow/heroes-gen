40497206020b3b66e9ee37ea615a8da85777fdf8
Vitalijs Litvinovs <dev@codin.pro>
2017-06-06 12:35:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <?php
 $source = preg_replace('/[^-a-z]/i', '', $_GET['src'] ?? 'marvel');
+$sourceFile = $source . '.json';
 
-$heroesFile = file_get_contents($source . '.json');
-$heroes = json_decode($heroesFile);
+if(file_exists($sourceFile)){
+    $heroesFile = file_get_contents($sourceFile);
+    $heroes = json_decode($heroesFile);
 
-$filterList = [
-    'lowercase' => function($name){ return strtolower($name); },
-    'uppercase' => function($name){ return strtoupper($name); },
-    'join' => function($name){ return str_replace(' ', '-', $name); },
-    'alpha' => function($name){ return preg_replace('/[^-a-z]|(?:[^a-z]$)/i', '', $name); },
-];
+    $filterList = [
+        'lowercase' => function($name){ return strtolower($name); },
+        'uppercase' => function($name){ return strtoupper($name); },
+        'join' => function($name){ return str_replace(' ', '-', $name); },
+        'alpha' => function($name){ return preg_replace('/[^-a-z]|(?:[^a-z]$)/i', '', $name); },
+    ];
 
-$hero = $heroes[mt_rand(0, count($heroes) - 1)];
+    $hero = $heroes[mt_rand(0, count($heroes) - 1)];
 
-if(isset($_GET['filters'])){
-    $filters = explode(',', $_GET['filters']);
-    foreach($filters as $filter){
-        if(isset($filterList[$filter])){
-            // We can't do `$hero = $filterList[$filter]($hero);`
-            $activeFilter = $filterList[$filter];
-            $hero = $activeFilter($hero);
+    if(isset($_GET['filters'])){
+        $filters = explode(',', $_GET['filters']);
+        foreach($filters as $filter){
+            if(isset($filterList[$filter])){
+                // We can't do `$hero = $filterList[$filter]($hero);`
+                $activeFilter = $filterList[$filter];
+                $hero = $activeFilter($hero);
+            }
         }
     }
-}
 
-header('Content-Type: application/json');
-die(json_encode($hero));
+    header('Content-Type: application/json');
+    die(json_encode($hero));
+}
