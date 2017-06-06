cf400d7e5840c512ee471d35821ba044dbe43416
Vitalijs Litvinovs <dev@codin.pro>
2017-06-06 12:13:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,23 @@
 $heroesFile = file_get_contents('heroes.json');
 $heroes = json_decode($heroesFile);
 
+$filterList = [
+    'lowercase' => function($name){ return strtolower($name); }, 
+    'uppercase' => function($name){ return strtoupper($name); }, 
+    'alpha' => function($name){ return preg_replace('/[^a-z]/gi', '', $name); },
+];
+
+$hero = $heroes[mt_rand(0, count($heroes) - 1)];
+
+if(isset($_GET['filters'])){
+    $filters = explode(',', $_GET['filters']);
+    
+    foreach($filters as $filter){
+        if(isset($filterList[$filter])){
+            $hero = $filterList[$filter]($hero);
+        }
+    }
+}
+
 header('Content-Type: application/json');
-die(json_encode($heroes[mt_rand(0, count($heroes) - 1)]));
+die(json_encode($hero));
